f868e8c90e4ba326fcd844151e235ac539de6660
AyzeGaming <35877385+AyzeGamingFR@users.noreply.github.com>
2021-02-18 18:00:55 +0100

    Created index.js

---
+++ index.js
@@ -0,0 +1,66 @@
+// default requires
+
+var fs = require("fs");
+var http = require("http");
+var https = require("https");
+var url = require("url");
+
+
+// default functions
+
+function receivingNewConnection() {
+    
+    
+    
+};
+
+function sendingNewConnection() {
+    
+    
+    
+};
+
+function receivingDatasFromWallet() {
+    
+    
+    
+};
+
+function sendingDatasToWallet() {
+    
+    
+    
+};
+
+function creatingAccount() {
+    
+    
+    
+};
+
+function deletingAccount() {
+    
+    
+    
+};
+
+function creatingWorker() {
+    
+    
+    
+};
+
+function deletingWorker() {
+    
+    
+    
+};
+
+
+// main function
+
+function main() {
+    
+    
+    
+};
