8c8a0fe91be8d0c4549d726a3c34aaaa9c797d32
AyzeGaming <35877385+AyzeGamingFR@users.noreply.github.com>
2021-02-20 11:53:26 +0100

    Created firewall.js

---
+++ firewall/firewall.js
@@ -0,0 +1,33 @@
+// default requires
+
+var fs = require("fs");
+
+var ipsDb = require("./ipsDb.json");
+
+
+// firewall functions
+
+function lookInDatabaseAllowedIps() {
+    
+    
+    
+};
+function lookInDatabaseBannedIps() {
+    
+    
+    
+};
+function lookInDatabaseWhitelistedIps() {
+    
+    
+    
+};
+
+
+// firewall function
+
+function firewall() {
+    
+    
+    
+};
