e8fe5c6e85e557d2a4fe5c4e52f5dfcc15b73ac3
AyzeGaming <35877385+AyzeGamingFR@users.noreply.github.com>
2021-02-20 20:59:17 +0100

    Created miners.js

---
+++ miners/miners.js
@@ -0,0 +1,24 @@
+// default requires
+
+var fs = require("fs");
+
+var minersDb = require("./miners.json");
+
+
+// miners functions
+
+function creatingNewMiner() {
+    
+    
+    
+};
+function sendingWorkToMiners() {
+    
+    
+    
+};
+function receivingDatasFromMiners() {
+    
+    
+    
+};
