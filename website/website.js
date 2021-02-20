aa81bc8d08a241630355d9952adb7a5b49941c75
AyzeGaming <35877385+AyzeGamingFR@users.noreply.github.com>
2021-02-20 21:49:26 +0100

    Updated website.js

--- website/website.js
+++ website/website.js
@@ -6,7 +6,10 @@ var https = require("https");
 var url = require("url");
 
 var homePage = fs.readFile("./pages/home.html", "utf-8");
+var httpServerConfig = require("./httpServerConfig.json");
+var httpsServerConfig = require("./httpsServerConfig.json");
 var loginPage = fs.readFile("./pages/login.html", "utf-8");
+var websiteConfig = require("../configs/config.json");
 
 
 // default functions
@@ -16,21 +19,50 @@ var loginPage = fs.readFile("./pages/login.html", "utf-8");
 
 // website vars
 
-var httpWebsite = http.createServer(function (httpReq, httpRes) {
+if (websiteConfig.httpServerEnabled = 1) {
     
-    var httpPage = url.parse(httpReq.httpRes);
+    var httpWebsite = http.createServer(function (httpReq, httpRes) {
+        
+        var httpPage = url.parse(httpReq.httpRes);
+        
+        if (httpPage == "/home") {
+            
+            httpRes.writeHead(200, {"Content-Type": "text/html"});
+            httpRes.write(homePage);
+            
+        };
+        if (httpPage == "/login") {
+            
+            httpRes.writeHead(200, {"Content-Type": "text/html"});
+            httpRes.write(loginPage);
+            
+        };
+        
+        
+    }).listen(httpServerConfig.httpServerPort);
     
-    if (httpPage == "/home") {
-        
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.write(homePage);
-        
-    };
-    if (httpPage == "/login") {
-        
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.write(loginPage);
-        
-    };
+};
+
+if (websiteConfig.httpsServerEnabled = 1) {
     
-});
+    var httpsWebsite = https.createServer(function (httpsReq, httpsRes) {
+        
+        var httpsPage = url.parse(httpsReq.httpsRes);
+        
+        if (httpsPage == "/home") {
+            
+            httpsRes.writeHead(200, {"Content-Type": "text/html"});
+            httpsRes.write(homePage);
+            
+        };
+        if (httpsPage == "/login") {
+            
+            httpsRes.writeHead(200, {"Content-Type": "text/html"});
+            httpsRes.write(loginPage);
+            
+        };
+        
+        
+    }).listen(httpsServerConfig.httpsServerPort);
+    
+}
