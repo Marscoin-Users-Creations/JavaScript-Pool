f904b84cea613b733dd99bbad2c04f306b1020a5
AyzeGaming <35877385+AyzeGamingFR@users.noreply.github.com>
2021-02-20 21:38:25 +0100

    Updated index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var url = require("url");
 var marscoinPoolConfig = require("./configs/marscoin.json");
 var firewall = require("./firewall/firewall.js");
 var miners = require("./miners.js");
+var wallet = require("./wallet/wallet.js");
 var website = require("./website/website.js");
 
 
