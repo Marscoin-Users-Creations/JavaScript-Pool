02469093f8d2b4685b779beeef983539dd36eed4
AyzeGaming <35877385+AyzeGamingFR@users.noreply.github.com>
2021-02-23 20:03:22 +0100

    Created stratum.js

---
+++ stratum/stratum.js
@@ -0,0 +1,36 @@
+// default requires
+
+
+
+
+// default functions
+
+function receivingConnection() {
+    
+    
+    
+};
+function sendingConnection() {
+    
+    
+    
+};
+function receivingDatas() {
+    
+    
+    
+};
+function sendingDatas() {
+    
+    
+    
+};
+
+
+// stratum function
+
+function stratum() {
+    
+    
+    
+};
