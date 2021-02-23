3a2e435d332230be3ab1cff6d3f177fe15c20a49
AyzeGaming <35877385+AyzeGamingFR@users.noreply.github.com>
2021-02-23 09:31:58 +0100

    Created databases.js

---
+++ databases/databases.js
@@ -0,0 +1,39 @@
+// default requires
+
+var logsDb = require("./logs.sql");
+var miningDb = require("./mining.sql");
+var paymentDb = require("./payment.sql");
+var usersDb = require("./users.sql");
+
+
+// default functions
+
+function getLogs() {
+    
+    
+    
+};
+function getMiningRewards() {
+    
+    
+    
+};
+function getPayments() {
+    
+    
+    
+};
+function getUsers() {
+    
+    
+    
+};
+
+
+// databases function
+
+function databases() {
+    
+    
+    
+};
