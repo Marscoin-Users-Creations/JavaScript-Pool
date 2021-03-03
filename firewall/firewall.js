f0b250ec42bd35a15d76ba1d300155ec864df5cd
AyzeGaming <35877385+AyzeGamingFR@users.noreply.github.com>
2021-03-03 13:00:58 +0100

    Updated firewall.js

--- firewall/firewall.js
+++ firewall/firewall.js
@@ -2,6 +2,7 @@
 
 var fs = require("fs");
 
+var bannedIps = require("../ips/banned.json");
 var ipsDb = require("./ipsDb.json");
 
 
@@ -23,6 +24,11 @@ function lookInDatabaseWhitelistedIps() {
 
 function firewall() {
     
-    
+    var newConnection = "";
+    if (newConnection.is_in(bannedIps.ipsBanned)) {
+        
+        
+        
+    };
     
 };
