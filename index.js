c76b8ae09dea9317ddc334fa2c988a1c8193a48c
AyzeGaming <35877385+AyzeGamingFR@users.noreply.github.com>
2021-03-21 18:57:54 +0100

    Updated index.js

--- index.js
+++ index.js
@@ -7,75 +7,27 @@ var algorithms = require("./algorithms/algorithms.js");
 var poolConfig = require("./configs/poolConfig.json");
 var databases = require("./databases/databases.js");
 var firewall = require("./firewall/firewall.js");
+var internet = require("./internet/internet.js");
 var miners = require("./miners.js");
 var wallet = require("./wallet/wallet.js");
 var website = require("./website/website.js");
 
 
-// default functions
+// main vars
 
-function receivingNewConnection() {
-    
-    
-    
-};
-function sendingNewConnection() {
-    
-    
-    
-};
-function receivingDatasFromWallet() {
-    
-    
-    
-};
-function sendingDatasToWallet() {
-    
-    
-    
-};
-function creatingAccount() {
-    
-    
-    
-};
-function deletingAccount() {
-    
-    
-    
-};
-function creatingWorker() {
-    
-    
-    
-};
-function deletingWorker() {
-    
-    
-    
-};
-function addingCoinsToAccount() {
-    
-    
-    
-};
-function removingCoinsToAccount() {
-    
-    
-    
-};
-function sendingCoinsToAddress() {
-    
-    
-    
-};
+var actualBlockDatas = "none";
+var blocksMining = 0;
+var poolCanBeStarted = 1;
 
 
 // main function
 
 function main() {
     
-    var actualBlock = "none";
-    var blocksMining = 0;
+    if (poolCanBeStarted = 1) {
+        
+        
+        
+    };
     
 };
